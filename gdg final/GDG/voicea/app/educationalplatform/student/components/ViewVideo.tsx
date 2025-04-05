d99c432b6f7c1903b47aa42a6eb16a34706a6b04
nikitabg07 <nikitagholap8706@gmail.com>
2025-04-06 01:43:48 +0530

    Update ViewVideo.tsx

--- gdg final/GDG/voicea/app/educationalplatform/student/components/ViewVideo.tsx
+++ gdg final/GDG/voicea/app/educationalplatform/student/components/ViewVideo.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import VideoPlayer from "../../components/VideoPlayer"; 
 
-const API_URL = "http://localhost:5000/api/videos";
+const API_URL = "https://voicea-back-ldg3.onrender.com/api/videos";
 
 const StudentViewVideos = () => {
   const [videos, setVideos] = useState<any[]>([]);
